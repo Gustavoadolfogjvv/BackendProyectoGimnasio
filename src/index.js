67a33e3b763f00bc2f363f6c17253bfa912b65ed
Gustavoadolfogjvv <Gustavoavilloldo@gmail.com>
2023-12-03 19:24:11 -0300

    add configurations middleware

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import Express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path';
 
 const app = Express();
 
@@ -18,3 +21,11 @@ app.get('/', (req, res) => {
 app.delete('/borrarProducto', (req, res) => {
     res.send('El producto fue borrado');
 })
+
+app.use(morgan('dev'));
+app.use(cors())
+app.use(Express.json());
+app.use(Express.urlencoded({ extended: true }));
+
+//app.use(Express.static('public'));
+app.use(Express.static(path.join(__dirname, '../public'))); 
